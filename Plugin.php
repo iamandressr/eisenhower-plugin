5c11aa671847eae7d29c66acdde91b14fca6dbbb
Andres Silva Roa <andres.silvar@dgac.gob.cl>
2025-07-18 11:38:37 -0400

    se agrega controller en plugin.php

--- Plugin.php
+++ Plugin.php
@@ -3,6 +3,7 @@
 namespace Kanboard\Plugin\Eisenhower;
 
 use DateTime;
+use Kanboard\Controller\BacklogBoardController;
 use Kanboard\Core\Plugin\Base;
 use Kanboard\Model\ProjectModel;
 use Kanboard\Plugin\Eisenhower\Model\ProjectUsesBacklogBoardModel;
