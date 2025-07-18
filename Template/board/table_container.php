d9b38e2f0790673fce37ded1e4601d404e5a0e9a
Andres Silva Roa <andres.silvar@dgac.gob.cl>
2025-07-18 11:52:49 -0400

    cambios en table_container

--- Template/board/table_container.php
+++ Template/board/table_container.php
@@ -1,123 +1,41 @@
-<?php // ADDED BY FL
+<style>
+.eisenhower-board {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto auto;
+    gap: 1rem;
+}
+.eisenhower-quadrant {
+    border: 1px solid #ccc;
+    padding: 10px;
+    min-height: 200px;
+}
+.eisenhower-quadrant h3 {
+    margin-top: 0;
+    font-size: 1.2em;
+}
+</style>
 
-$backloglane = $swimlanes[0];
-// $backloglane['columns'][0]['title'] = 'Backlog';
-$backloglane['columns'][0]['title'] = $this->task->configModel->get('backlog_board_title', 'Backlog');
-$backlogcolumn = $backloglane['columns'][0];
-$backloglane['columns'] = array();
-$backloglane['columns'][] = $backlogcolumn;
-$UseBacklog = FALSE;
-$backlogwidth = 100 / $backloglane['nb_columns'];
-$board_class = '';
-?>
-<?php if ($this->task->projectUsesBacklogBoardModel->backlogIsset($project['id'])) {
-    $UseBacklog = TRUE;
-    $board_class = ' backlog-enabled';
-} ?>
+<div class="eisenhower-board">
 
-<div id="board-container">
-    <?php if (empty($swimlanes) || empty($swimlanes[0]['nb_columns'])): ?>
-        <p class="alert alert-error"><?= t('There is no column or swimlane activated in your project!') ?></p>
-    <?php else: ?>
+    <div class="eisenhower-quadrant" id="do-now">
+        <h3><?= t('Urgente e Importante') ?> (Hacer ahora)</h3>
+        <?= $this->render('eisenhower:board/tasks', ['tasks' => $tasks_by_priority[3], 'project' => $project]) ?>
+    </div>
 
-    <?php if (isset($not_editable)): ?>
-    <table id="board" class="board-project-<?= $project['id'] ?><?= $board_class ?>">
-        <?php else: ?>
-        <table id="board"
-               class="board-project-<?= $project['id'] ?><?= $board_class ?>"
-               data-project-id="<?= $project['id'] ?>"
-               data-check-interval="<?= $board_private_refresh_interval ?>"
-               data-save-url="<?= $this->url->href('BoardAjaxController', 'save', array('project_id' => $project['id'])) ?>"
-               data-reload-url="<?= $this->url->href('BoardAjaxController', 'reload', array('project_id' => $project['id'])) ?>"
-               data-check-url="<?= $this->url->href('BoardAjaxController', 'check', array('project_id' => $project['id'], 'timestamp' => time())) ?>"
-               data-task-creation-url="<?= $this->url->href('TaskCreationController', 'show', array('project_id' => $project['id'])) ?>"
-        >
-            <?php endif ?>
+    <div class="eisenhower-quadrant" id="schedule">
+        <h3><?= t('No urgente pero Importante') ?> (Planificar)</h3>
+        <?= $this->render('eisenhower:board/tasks', ['tasks' => $tasks_by_priority[2], 'project' => $project]) ?>
+    </div>
 
-            <?php if ($UseBacklog === TRUE): ?>
-            <tbody>
-            <tr>
-                <td id="kanboard-column" style="min-width:240px; width:<?php print $backlogwidth; ?>%;" data-nb_columns="<?php print $backloglane['nb_columns']; ?>">
-                    <table id="backlog-board">
+    <div class="eisenhower-quadrant" id="delegate">
+        <h3><?= t('Urgente pero No importante') ?> (Delegar)</h3>
+        <?= $this->render('eisenhower:board/tasks', ['tasks' => $tasks_by_priority[1], 'project' => $project]) ?>
+    </div>
 
-                        <?= $this->render('board/table_column', array(
-                            'swimlane' => $backloglane,
-                            'not_editable' => isset($not_editable),
-                        )) ?>
-                        <?= $this->render('board/table_tasks', array(
-                            'project' => $project,
-                            'swimlane' => $backloglane,
-                            'not_editable' => isset($not_editable),
-                            'board_highlight_period' => $board_highlight_period,
-                        )) ?>
+    <div class="eisenhower-quadrant" id="eliminate">
+        <h3><?= t('No urgente ni Importante') ?> (Eliminar)</h3>
+        <?= $this->render('eisenhower:board/tasks', ['tasks' => $tasks_by_priority[0], 'project' => $project]) ?>
+    </div>
 
-                    </table>
-                </td>
-                <td>
-                    <table id="main-board">
-
-                        <?php array_shift($swimlanes); ?>
-                        <?php endif ?>
-                        <?php // END ADDED ?>
-
-
-
-
-                        <?php foreach ($swimlanes as $index => $swimlane): ?>
-
-                            <?php // ADDED BY FL ?>
-                            <?php if ($UseBacklog === TRUE): ?>
-                                <?php
-                                unset($swimlane['columns'][0]);
-                                $swimlane['nb_columns'] = $swimlane['nb_columns'] - 1;
-                                ?>
-                            <?php endif ?>
-                            <?php // END ADDED ?>
-
-                            <?php if (!($swimlane['nb_tasks'] === 0 && isset($not_editable))): ?>
-
-                                <!-- Note: Do not show swimlane row on the top otherwise we can't collapse columns -->
-                                <?php if ($index > 0 && $swimlane['nb_swimlanes'] > 1): ?>
-                                    <?= $this->render('board/table_swimlane', array(
-                                        'project' => $project,
-                                        'swimlane' => $swimlane,
-                                        'not_editable' => isset($not_editable),
-                                    )) ?>
-                                <?php endif ?>
-
-                                <?= $this->render('board/table_column', array(
-                                    'swimlane' => $swimlane,
-                                    'not_editable' => isset($not_editable),
-                                )) ?>
-
-                                <?php if ($index === 0 && $swimlane['nb_swimlanes'] > 1): ?>
-                                    <?= $this->render('board/table_swimlane', array(
-                                        'project' => $project,
-                                        'swimlane' => $swimlane,
-                                        'not_editable' => isset($not_editable),
-                                    )) ?>
-                                <?php endif ?>
-
-                                <?= $this->render('board/table_tasks', array(
-                                    'project' => $project,
-                                    'swimlane' => $swimlane,
-                                    'not_editable' => isset($not_editable),
-                                    'board_highlight_period' => $board_highlight_period,
-                                )) ?>
-
-                            <?php endif ?>
-                        <?php endforeach ?>
-
-                        <?php // ADDED BY FL ?>
-                        <?php if ($UseBacklog === TRUE): ?>
-                    </table>
-                </td>
-            </tr>
-            </tbody>
-        <?php endif ?>
-            <?php // END ADDED BY FL ?>
-
-        </table>
-
-        <?php endif ?>
 </div>
