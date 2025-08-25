b1c8882912e42853ed5385c4e5f3e4f2c3035255
Andres Silva Roa <andres.silvar@dgac.gob.cl>
2025-08-25 12:04:36 -0400

    cambios

--- Template/board/table_container.php
+++ Template/board/table_container.php
@@ -15,33 +15,26 @@ foreach ($tasks as $task) {
     }
     $tasks_by_priority[$priority][] = $task;
 }
+
+// Mapea prioridad a columna y swimlane del Eisenhower Board
+$quadrant_mapping = [
+    3 => ['column_id' => $do_now_column_id, 'swimlane_id' => $do_now_swimlane_id],
+    2 => ['column_id' => $schedule_column_id, 'swimlane_id' => $schedule_swimlane_id],
+    1 => ['column_id' => $delegate_column_id, 'swimlane_id' => $delegate_swimlane_id],
+    0 => ['column_id' => $eliminate_column_id, 'swimlane_id' => $eliminate_swimlane_id],
+];
 ?>
 
 <hr>
 
-<div style="display: flex; gap: 10px; margin-bottom: 15px;">
-    <a href="/?controller=TaskCreationController&amp;action=show&amp;project_id=1" class="js-modal-large" title=""><i class="fa fa-plus fa-fw js-modal-large" aria-hidden="true"></i>Añadir una nueva tarea</a>
-
-    <p>|</p>
-
-    <a href="/?controller=ExternalTaskCreationController&amp;action=step1&amp;project_id=1&amp;swimlane_id=2&amp;column_id=2&amp;provider_name=Mantis" class="js-modal-large btn-add-mantis" title="">
-        <i class="fa fa-bug fa-fw" aria-hidden="true"></i> Añadir Tarea de Mantis
-    </a>
-</div>
-
 <div class="eisenhower-container">
-
-    <div class="corner"></div>
-    <div class="urgente"><?= t('Urgente') ?></div>
-    <div class="nourgente"><?= t('No urgente') ?></div>
-    <div class="importante"><?= t('Importante') ?></div>
 
     <?php
     $colors = [
-        3 => '#ffb3b3', // Hacer ahora
-        2 => '#ffd9b3', // Planificar
-        1 => '#ffffb3', // Delegar
-        0 => '#b3ffb3'  // Eliminar
+        3 => '#ffb3b3',
+        2 => '#ffd9b3',
+        1 => '#ffffb3',
+        0 => '#b3ffb3'
     ];
 
     $quadrants = [
@@ -51,12 +44,16 @@ foreach ($tasks as $task) {
         0 => ['id' => 'eliminate', 'title' => t('Eliminar (0)')],
     ];
 
-    foreach ($quadrants as $priority => $info): ?>
+    foreach ($quadrants as $priority => $info):
+        $columnId = $quadrant_mapping[$priority]['column_id'];
+        $swimlaneId = $quadrant_mapping[$priority]['swimlane_id'];
+    ?>
     <div class="eisenhower-quadrant"
          id="<?= $info['id'] ?>"
-         style="grid-area: <?= $info['id'] ?>;
-         background-color: <?= $colors[$priority] ?>;"
-         data-priority="<?= $priority ?>">
+         style="grid-area: <?= $info['id'] ?>; background-color: <?= $colors[$priority] ?>;"
+         data-priority="<?= $priority ?>"
+         data-column-id="<?= $columnId ?>"
+         data-swimlane-id="<?= $swimlaneId ?>">
 
         <h4><?= $info['title'] ?></h4>
 
@@ -65,51 +62,20 @@ foreach ($tasks as $task) {
                  data-task-id="<?= $task['id'] ?>"
                  data-column-id="<?= $task['column_id'] ?>"
                  data-swimlane-id="<?= $task['swimlane_id'] ?>"
-                 data-position="<?= $task['position'] ?>"
-            >
-                <div class="task-card-header" style="display: flex; justify-content: space-between; align-items: center;">
-                    <?= $this->url->link(
-                        '<strong>' . $this->text->e($task['title']) . '</strong>',
-                        'TaskViewController',
-                        'show',
-                        ['task_id' => $task['id'], 'project_id' => $project['id']],
-                        false
-                    ) ?>
-
-                    <div style="display: flex; gap: 10px;">
-                        <a href="<?= $this->url->href('TaskModificationController', 'edit', ['task_id' => $task['id']]) ?>"
-                           class="js-modal-large"
-                           title="<?= t('Editar tarea') ?>">
-                            <i class="fa fa-edit fa-fw" aria-hidden="true"></i> <?= t('Editar') ?>
-                        </a>
-
-                        <p> | </p>
-
-                        <a href="<?= $this->url->href('TaskSuppressionController', 'confirm', ['task_id' => $task['id'], 'redirect' => 'board']) ?>"
-                           class="js-modal-confirm"
-                           title="<?= t('Suprimir tarea') ?>">
-                            <i class="fa fa-trash-o fa-fw" aria-hidden="true"></i> <?= t('Eliminar') ?>
-                        </a>
-                    </div>
-                </div>
-
-                <?php if (!empty($task['assignee_name'])): ?>
-                    <small><?= t('Asignado a') ?>: <?= $this->text->e($task['assignee_name']) ?></small>
-                <?php endif ?>
+                 data-position="<?= $task['position'] ?>">
+                <!-- tu HTML de task-card aquí -->
             </div>
         <?php endforeach ?>
     </div>
-<?php endforeach ?>
-
-    <div class="noimportante"><?= t('No importante') ?></div>
+    <?php endforeach ?>
 </div>
 
 <div id="eisenhower-config"
      data-csrf-token="<?= $this->app->config('csrf_token') ?>"
      data-update-priority-url="<?= $this->url->href('BacklogBoardController', 'updatePriority', [], 'Eisenhower') ?>"
      data-move-task-url="<?= $this->url->href('BacklogBoardController', 'moveTask', [], 'Eisenhower') ?>">
-
 </div>
+
 
 <script src="<?= $this->url->dir() ?>plugins/Eisenhower/Assets/backlog.js"></script>
 <style src="<?= $this->url->dir() ?>plugins/Eisenhower/Assets/backlog.css></style> 
