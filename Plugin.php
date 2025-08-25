473874c5c13c0d1df62c5693c96a077eb3c8a59b
Andres Silva Roa <andres.silvar@dgac.gob.cl>
2025-08-25 10:58:56 -0400

    linea comentada plugin.php

--- Plugin.php
+++ Plugin.php
@@ -18,7 +18,7 @@ class Plugin extends Base
     {
         
         $this->template->setTemplateOverride('board/table_container','eisenhower:board/table_container');
-        $this->template->setTemplateOverride('column/index','eisenhower:column/index');
+        //$this->template->setTemplateOverride('column/index','eisenhower:column/index');
         $this->template->setTemplateOverride('swimlane/table','eisenhower:swimlane/table');
         $this->hook->on('template:layout:js', array('template' => 'plugins/Eisenhower/Assets/backlog.js'));
         $this->hook->on('template:layout:css', array('template' => 'plugins/Eisenhower/Assets/backlog.css'));
